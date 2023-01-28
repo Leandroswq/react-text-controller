f5d61ece76c1d1fff3d49c2e736a8570aecb8e30
Leandroswq <leandrobonfim15@hotmail.com>
2023-01-27 23:20:56 -0300

    feat: add view to TextController

--- example/src/App.tsx
+++ example/src/App.tsx
@@ -1,9 +1,48 @@
-import React from 'react';
+import React, { useState } from 'react';
+import { TextController } from '@Leandroswq/react-text-controller';
 
 function App() {
+  const [states, setStates] = useState([
+    {
+      key: 'header',
+      value: 'Meu titulo',
+      isActive: false,
+    },
+    {
+      key: 'paragrafo',
+      value: 'meu paragrafo',
+      isActive: false,
+    },
+  ]);
+
+  function sendSate() {
+    console.log('Enviando para o servidor', states);
+  }
+
   return (
     <div className="App">
-      <p>teste</p>
+      <TextController
+        states={states}
+        updateStates={setStates}
+        sendButtonFunction={sendSate}
+      />
+      {states.map((state) => (
+        <div key={state.key} style={state.isActive ?{
+          border: '3px solid red',
+          width: '30%',
+          margin: '30px'
+        }:
+        {
+          border: '3px solid black',
+          width: '30%',
+          margin: '30px'
+        }
+        }>
+          <span>{state.key}</span>:
+          <span style={{ color: 'red' }}>{state.value}</span>:
+          <span>{state.isActive ? 'true' : 'false'}</span>
+        </div>
+      ))}
     </div>
   );
 }
